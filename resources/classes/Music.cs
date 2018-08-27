1e337419927fcb82be832aaf43e412c6bc043ec5
Rodney McQuain <rodneymcquain95@gmail.com>
2018-08-26 23:34:09 -0400

    Added userID instance field

--- resources/classes/Music.cs
+++ resources/classes/Music.cs
@@ -8,18 +8,21 @@ namespace musicP
     public class Music
     {
         public int musicID { get; }
+        public int userID { get; }
         public string artist { get; }
         public string album { get; }
 
-        public Music(int musicID, string artist, string album)
+        public Music(int musicID, int userID, string artist, string album)
         {
             this.musicID = musicID;
+            this.userID = userID;
             this.artist = artist;
             this.album = album;
         }
 
-        public Music(string artist, string album)
+        public Music(int userID, string artist, string album)
         {
+            this.userID = userID;
             this.artist = artist;
             this.album = album;
         }
