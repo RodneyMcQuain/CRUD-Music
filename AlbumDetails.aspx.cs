671d54c7aaacafa8dd281b391504bb08e5f2545f
Rodney McQuain <rodneymcquain95@gmail.com>
2018-08-28 13:26:41 -0400

    Updated with year and genre changes

--- AlbumDetails.aspx.cs
+++ AlbumDetails.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using musicP.resources.database;
+using musicP.resources.utilities;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -13,7 +14,6 @@ namespace musicP
     {
         private IMusicDAO musicDao = new MusicDAOImpl();
 
-
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -24,35 +24,55 @@ namespace musicP
                 lblArtistAlbumAlbumDetails.Text = music.ToString();
                 tbArtistAlbumDetails.Text = music.artist;
                 tbAlbumAlbumDetails.Text = music.album;
+                tbYear.Text = music.year.ToString();
+                tbGenre.Text = music.genre;
             }
-         }
+        }
 
         protected void btUpdate_Click(object sender, EventArgs e)
         {
-            if (tbArtistAlbumDetails.Text.Length == 0 || tbAlbumAlbumDetails.Text.Length == 0)
+            if (GUIUtils.EmptyControl(tbArtistAlbumDetails) ||
+                GUIUtils.EmptyControl(tbAlbumAlbumDetails))
             {
-                Label lblModalTitle = (Label)Master.FindControl("lblModalTitle");
-                lblModalTitle.Text = "Error";
-
-                Label lblModalBody = (Label)Master.FindControl("lblModalBody");
-                lblModalBody.Text = "The artist and album fields must be filled.";
-
-                Button btModalButton1 = (Button)Master.FindControl("btModalButton1");
-                btModalButton1.Text = "Okay";
-
-                Button btModalCloseButton = (Button)Master.FindControl("btModalCloseButton");
-                btModalCloseButton.Text = "Close";
-
-                ScriptManager.RegisterStartupScript(Page, Page.GetType(), "aModal", "$('#aModal').modal();", true);
+                Alert("Empty Error", "The artist and album fields must be filled.");
                 return;
             }
 
-            int id = Int32.Parse(Request.QueryString["Id"]);
-            Music music = new Music(id, tbArtistAlbumDetails.Text, tbAlbumAlbumDetails.Text);
+            int year;
+            if (!int.TryParse(tbYear.Text, out year))
+            {
+                Alert("Parse Error", "The year field must be an integer.");
+                return;
+            }
+
+            int musicID = Int32.Parse(Request.QueryString["Id"]);
+            int userID = Convert.ToInt32(Session["userID"]);
+            string artist = tbArtistAlbumDetails.Text;
+            string album = tbAlbumAlbumDetails.Text;
+            string genre = tbGenre.Text;
+
+            Music music = new Music(musicID, userID, artist, album, year, genre);
 
             musicDao.UpdateMusic(music);
 
             lblArtistAlbumAlbumDetails.Text = music.ToString();
+        }
+
+        private void Alert(string title, string body)
+        {
+            Label lblModalTitle = (Label)Master.FindControl("lblModalTitle");
+            lblModalTitle.Text = title;
+
+            Label lblModalBody = (Label)Master.FindControl("lblModalBody");
+            lblModalBody.Text = body;
+
+            LinkButton btModalButton1 = (LinkButton)Master.FindControl("btModalButton1");
+            btModalButton1.Text = "Okay";
+
+            LinkButton btModalCloseButton = (LinkButton)Master.FindControl("btModalCloseButton");
+            btModalCloseButton.Text = "Close";
+
+            ScriptManager.RegisterStartupScript(Page, Page.GetType(), "aModal", "$('#aModal').modal();", true);
         }
 
         protected void btDelete_Click(object sender, EventArgs e)
