3cdd3e17aea96afce29e1687901893f85e445a89
Rodney McQuain <rodneymcquain95@gmail.com>
2018-08-28 12:08:21 -0400

    Added year and genre instance fields

--- resources/classes/Music.cs
+++ resources/classes/Music.cs
@@ -11,20 +11,26 @@ namespace musicP
         public int userID { get; }
         public string artist { get; }
         public string album { get; }
+        public int year { get; }
+        public string genre { get; }
 
-        public Music(int musicID, int userID, string artist, string album)
+        public Music(int musicID, int userID, string artist, string album, int year, string genre)
         {
             this.musicID = musicID;
             this.userID = userID;
             this.artist = artist;
             this.album = album;
+            this.year = year;
+            this.genre = genre;
         }
 
-        public Music(int userID, string artist, string album)
+        public Music(int userID, string artist, string album, int year, string genre)
         {
             this.userID = userID;
             this.artist = artist;
             this.album = album;
+            this.year = year;
+            this.genre = genre;
         }
 
         override
