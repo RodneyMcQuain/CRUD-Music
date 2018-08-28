1ed4fdf7f3e4999973f4be1d494e160dd624806a
Rodney McQuain <rodneymcquain95@gmail.com>
2018-08-28 13:07:52 -0400

    Initial Commit

---
+++ resources/utilities/GUIUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace musicP.resources.utilities
+{
+    public class GUIUtils
+    {
+        public static bool EmptyControl(TextBox control)
+        {
+            if (control.Text.Trim().Equals(""))
+                return true;
+            else
+                return false;
+        }
+    }
+}
