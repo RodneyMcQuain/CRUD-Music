563fb5971aeab70537fc7aa2958fd0f49074e799
Rodney McQuain <rodneymcquain95@gmail.com>
2018-08-29 00:43:11 -0400

    Added id and server attribute to navbar container

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace musicP {
     public partial class SiteMaster {
         
         /// <summary>
+        /// navbar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl navbar;
+        
+        /// <summary>
         /// liHome control.
         /// </summary>
         /// <remarks>
